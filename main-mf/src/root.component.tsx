a3179f82c4ffa94a7c15624f5be312c902210768
NicolasHuertas <98675931+NicolasHuertas@users.noreply.github.com>
2023-11-26 15:25:52 -0500

    added main-mf content

--- main-mf/src/root.component.tsx
+++ main-mf/src/root.component.tsx
@@ -1,3 +1,28 @@
+import './styles.css';
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <html lang="en">
+
+        <div className="splash-container">
+          <div className="splash">
+            <h1 className="splash-head">Software Development 3</h1>
+            <p className="splash-subhead">
+              This is a micro-frontend based application made with single SPA, this particular component was made with React, while the navbar above was made with angular.
+            </p>
+            <p className="splash-subhead">
+              The technology was researched and implemented with the colaboration of all team members.
+            </p>
+          </div>
+        </div>
+
+        <div className="content-wrapper">
+          {/* ... (rest of your content) */}
+        </div>
+
+        <div className="footer l-box is-center">
+          View the source of this layout to learn more. Made with love by the Pure Team.
+        </div>
+    </html>
+  );
 }
